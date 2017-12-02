3427dcb370825a6dd6190377261128eb3b255c3e
peter soleeh <petersoleeh@gmail.com>
2017-12-02 10:59:53 +0300

    index setup url templates and views

--- carpool/rides/views.py
+++ carpool/rides/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
+@login_required
+def index(request):
+    return render(request,'index.html')
+
+
+def profile(request):
+    return render(request,'all-rides/user_profile.html')
