bb9ee2c176ec011afb75ba29a016c6a003bb92d6
peter soleeh <petersoleeh@gmail.com>
2017-12-02 11:01:19 +0300

    extend user profile

--- carpool/rides/models.py
+++ carpool/rides/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500,blank=True)
+    phone_number = models.IntegerField(blank=True,null=True)
+    pro_pic = models.ImageField(upload_to = 'rides/',blank=True,null=True)
+
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save,sender=User)
+def save_user_profile(sender,instance, **kwargs):
+    instance.profile.save()
