dc402be5bef0bb9e26e01b9366a9351f66b18ee2
peter soleeh <petersoleeh@gmail.com>
2017-12-03 15:19:33 +0300

    user and profile forms

---
+++ carpool/rides/forms.py
@@ -0,0 +1,15 @@
+from .models import Profile
+from django import forms
+from django.contrib.auth.models import User
+
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'phone_number', 'pro_pic')
